2fb8dc1ca52999c038f71d27f762b297a38a9a41
Kishik <kishikkhajuria03@gmail.com>
2026-03-14 22:01:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -58,3 +58,11 @@ function toggleTodo(index) {
     renderTodoList();
 }
 
+function getTodayDate() {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+}
+
