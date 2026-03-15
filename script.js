38d905f71603892b5bd3b43df38726cf87511a5e
Kishik <kishikkhajuria03@gmail.com>
2026-03-15 18:28:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function renderTodoList() {
             <span class="todo-name">${todo.name}</span>
             <span class="todo-date">${todo.dueDate ? '📅 ' + todo.dueDate : 'No due date'}</span>
         </div>
-        <button class="delete-btn">
+        <button class="delete-btn" onclick="deleteTodo(${index})">
             <svg width="20" height="20" fill="currentColor" viewBox="0 0 24 24">
                 <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/>
             </svg>
@@ -50,11 +50,16 @@ function addTodo() {
   renderTodoList();
 }
 
-/**
- * NEW: Toggle task completion
- */
 function toggleTodo(index) {
     todoList[index].completed = !todoList[index].completed;
+    renderTodoList();
+}
+
+/**
+ * NEW: Delete a specific task
+ */
+function deleteTodo(index) {
+    todoList.splice(index, 1);
     renderTodoList();
 }
 
